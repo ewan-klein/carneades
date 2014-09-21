77f41ea596ba05e94ccb55a77109ce1de9897cba
Ewan Klein <ewan@inf.ed.ac.uk>
2014-09-21 12:58:06 +0100

    Removed code for adding edges for negative conclusions
    Checked indentation

--- src/caes/caes.py
+++ src/caes/caes.py
@@ -37,7 +37,7 @@ Using a CAES
 >>> argset.add_argument(arg2)
 >>> argset.add_argument(arg3)
 >>> #argset.draw()
-    
+
 >>> assumptions = {kill, witness1, witness2, unreliable2}
 >>> weights = {'arg1': 0.8, 'arg2': 0.3, 'arg3': 0.8}
 >>> audience = Audience(assumptions, weights)
@@ -48,31 +48,27 @@ Using a CAES
 [witness1], ~[unreliable1] => intent
 [witness2], ~[unreliable2] => -intent
 
->>> len(argset.get_arguments(intent))
-1
 >>> arg_for_intent = argset.get_arguments(intent)[0]
 >>> print(arg_for_intent)
 [witness1], ~[unreliable1] => intent
 >>> caes.applicable(arg_for_intent)
 True
 
->>> len(argset.get_arguments(neg_intent))
-1
->>> arg_for_neg_intent = argset.get_arguments(neg_intent)[0]
->>> print(arg_for_neg_intent)
-[witness2], ~[unreliable2] => -intent
+Although there is an argument (arg3) for '-intent', it is not applicable,
+since `unreliable2` is an exception:
 
-This argument is not applicable, since `unreliable2` is an exception:
-
->>> caes.applicable(arg_for_neg_intent)
+>>> any(caes.applicable(arg) for arg in argset.get_arguments(neg_intent))
 False
-
-
+>>> caes.acceptable(neg_intent)
+False
 
 >>> caes.weight_of(arg2) > caes.alpha
 False
 
 >>> caes.acceptable(murder)
+False
+>>> caes.acceptable(murder.negate())
+False
 """
 
 
@@ -91,7 +87,7 @@ class PropLiteral(object):
     """
     Proposition literals have most of the properties of ordinary strings,
     except that the negation method is Boolean; i.e. 
-    
+
     >>> a = PropLiteral('a')
     >>> a.negate().negate() == a
     True
@@ -107,12 +103,12 @@ class PropLiteral(object):
     def negate(self):
         """
         Negation of a proposition.
-        
+
         We create a copy of the current proposition and flip its polarity.
         """
         polarity = (not self.polarity)
         return PropLiteral(self._string, polarity=polarity)
-    
+
 
     def __str__(self):
         """
@@ -162,7 +158,7 @@ class Argument(object):
         :type arg_id: None or str
         :param weight: The exceptions of the argument
         :type arg_id: None or str
-        
+
         """
 
         self.conclusion = conclusion
@@ -212,7 +208,7 @@ class ArgumentSet(object):
     def add_proposition(self, proposition):        
         """
         Add a proposition to a graph if it is not already present as a vertex.
-        
+
         :param prop: The proposition to be added to the graph.
         :type prop: :class:`PropLiteral`
         :return: The graph vertex corresponding to the proposition.
@@ -226,14 +222,14 @@ class ArgumentSet(object):
                 self.graph.add_vertex(prop=proposition)
                 logging.debug("Added proposition '{}' to graph".format(proposition))
             return self.graph.vs.select(prop=proposition)[0]                
-                
+
         else:
             raise TypeError('Input {} should be PropLiteral'.format(proposition))
 
     def add_argument(self, argument):
         """
         Add an argument to the graph.
-        
+
         :parameter argument: The argument to be added to the graph.
         :type argument: :class:`Argument`
         """
@@ -245,88 +241,79 @@ class ArgumentSet(object):
             arg_id = argument.arg_id
         self.arg_count += 1
         self.arguments.append(argument)
-        
+
         # add an argument vertex to the graph
         self.graph.add_vertex(arg=arg_id)
         arg_v = g.vs.select(arg=arg_id)[0]
-        
-        #conclusions = [argument.conclusion, argument.conclusion.negate()]     
-        #conclusion_vs = [self.add_proposition(conc) for conc in conclusions]        
-        #premise_vs = [self.add_proposition(prop) for prop in sorted(argument.premises)]
-        #exception_vs = [self.add_proposition(prop) for prop in sorted(argument.exceptions)]
-        #target_vs = premise_vs + exception_vs
-        
-        ## add new vertices to the graph
-        #edges_to_arg = [(conc_v.index, arg_v.index) for conc_v in conclusion_vs]
-        #edges_from_arg = [(arg_v.index, target.index) for target in target_vs]
-        #g.add_edges(edges_to_arg + edges_from_arg)
-            
-        conclusion_v = self.add_proposition(argument.conclusion)     
+
+        # add proposition vertices to the graph    
+        conclusion_v = self.add_proposition(argument.conclusion)
+        self.add_proposition(argument.conclusion.negate())
         premise_vs = [self.add_proposition(prop) for prop in sorted(argument.premises)]
         exception_vs = [self.add_proposition(prop) for prop in sorted(argument.exceptions)]
         target_vs = premise_vs + exception_vs
-        
-        # add new vertices to the graph
+
+        # add new edges to the graph
         edge_to_arg = [(conclusion_v.index, arg_v.index)]
         edges_from_arg = [(arg_v.index, target.index) for target in target_vs]
         g.add_edges(edge_to_arg + edges_from_arg)
-        
-        
+
+
     def get_arguments(self, proposition):
         """
         Find the arguments for a proposition in an *ArgumentSet*.
-        
+
         :param proposition: The proposition to be checked.
         :type proposition: :class:`PropLiteral`
         :return: A list of the arguments pro the proposition
         :rtype: list(:class:`Argument`)
         """
         g = self.graph
-        
+
         # index of vertex associated with the proposition
         conc_v_index = g.vs.select(prop=proposition)[0].index
-        
+
         # IDs of vertices reachable in one hop from the proposition's vertex
         target_IDs = [e.target for e in g.es.select(_source=conc_v_index)]
-        
+
         # the vertices indexed by target_IDs
         out_vs = [g.vs[i] for i in target_IDs]
-        
+
         arg_IDs = [v['arg'] for v in out_vs]
         args = [arg for arg in self.arguments if arg.arg_id in arg_IDs]
         return args
-        
+
 
 
     def draw(self, debug=False, roots=None):
         """
         Visualise an :class:`ArgumentSet` as a labeled graph.
-        
+
         :parameter debug: If :class:`True`, add the vertex index to the label.
         """
         g = self.graph
-        
-        
+
+
         # labels for nodes that are classed as propositions
         labels = g.vs['prop']
-        
+
         # insert the labels for nodes that are classed as arguments
         for i in range(len(labels)):
             if g.vs['arg'][i] is not None:
                 labels[i] = g.vs['arg'][i]
-                                
+
         if debug:
             d_labels = []
             for (i, label) in enumerate(labels):
                 d_labels.append("{}\nv{}".format(label, g.vs[i].index))
-                
+
             labels = d_labels
-            
+
         g.vs['label'] = labels
-            
+
         if roots is None:     
             roots = [i for i in range(len(g.vs)) if g.indegree()[i] == 0]
-            
+
         layout = g.layout_reingold_tilford(mode=ALL, root=roots)
 
         plot_style = {}
@@ -335,7 +322,7 @@ class ArgumentSet(object):
         plot_style['vertex_shape'] = ['circle' if x is None else 'rect' for x in g.vs['arg'] ]      
         plot_style['margin'] = 40
         plot_style['layout'] = layout
-        
+
         plot(g, **plot_style)
 
 
@@ -374,12 +361,12 @@ assigns weights to arguments.
 class CAES(object):
     """
     A class that represents a Carneades Argument Evaluation Structure (CAES).
- 
+
     """
     def __init__(self, argset, audience, proofstandard, alpha=0.4, beta=0.3,
                  gamma=0.2):
         """
-        
+
         :type argset: :class:`ArgSet`
         :type audience: :class:`Audience`
         :type standard: :class:`ProofStandard`
@@ -391,24 +378,24 @@ class CAES(object):
         self.alpha = alpha
         self.beta = beta
         self.gamma = gamma
-        
+
 
     def get_all_arguments(self):
         for arg in self.argset.arguments:
             print(arg)
-        
+
     @TraceCalls()    
     def applicable(self, argument):
         """
         An argument is *applicable* in a CAES if it needs to be taken into account when evaluating the CAES.
-        
+
         :parameter argument: The argument whose applicablility is being determined.
         :type argument: :class:`Argument`
         :rtype: bool
         """
         _acceptable = lambda p: self.acceptable(p)        
         return self._applicable(argument, _acceptable)
-                                  
+
     def _applicable(self, argument, _acceptable):
         """       
         :parameter argument: The argument whose applicablility is being determined.
@@ -422,32 +409,32 @@ class CAES(object):
         logging.debug('Current premises: {}'.format(argument.premises))
         b1 = all(p in self.assumptions or \
                  (p.negate() not in self.assumptions and _acceptable(p)) for p in argument.premises)
-        
+
         if argument.exceptions:
             logging.debug('Current exception: {}'.format(argument.exceptions))
         b2 = all(e not in self.assumptions and \
                  (e.negate() in self.assumptions or not _acceptable(e)) for e in argument.exceptions)
-        
+
         return b1 and b2
-       
-                
+
+
     @TraceCalls()
     def acceptable(self, proposition):
         """
         A conclusion is *acceptable* in a CAES if it can be arrived at under
         the relevant proof standards, given the beliefs of the audience.
         """
-        
+
         standard = self.standard.assign_standard(proposition)
         logging.debug("Checking whether proposition '{}' meets proof standard '{}'.".format(proposition, standard))
         return self.meets_proof_standard(proposition, standard)
-                
+
     @TraceCalls()
     def meets_proof_standard(self, proposition, standard):
         arguments = self.argset.get_arguments(proposition)
 
         result = False
-        
+
         if standard == 'scintilla':
             result = any(arg for arg in arguments if self.applicable(arg))            
         elif standard == 'preponderance':
@@ -456,24 +443,24 @@ class CAES(object):
             mwp = self.max_weight_pro(proposition)
             mwc = self.max_weight_con(proposition)
             logging.debug("    {}: weight pro '{}' >  weight con '{}'?".format(proposition, mwp, mwc))
-            
+
             result = (mwp > self.alpha) and (mwp - mwc > self.gamma)            
         elif standard == 'beyond_reasonable_doubt':
             result = self.meets_proof_standard(proposition, 'clear_and_convincing')\
                 and self.max_weight_con(proposition) < self.gamma            
 
-        
+
         #logging.debug("Proposition '{}' meets standard '{}': {}".format(proposition, standard, result))
         return result        
-        
+
     def weight_of(self, argument):
         arg_id = argument.arg_id
         try:
             return self.weight[arg_id]            
         except KeyError:
             raise ValueError("No weight assigned to argument '{}'.".format(arg_id))
-        
-        
+
+
     def max_weight_applicable(self, arguments):
         """
         :parameter arguments: The arguments whose weight is being compared.
@@ -487,24 +474,24 @@ class CAES(object):
         if len(applicable_args) == 0:
             logging.debug('No applicable arguments in {}'.format(arg_ids))
             return 0
-        
+
         applic_arg_ids = [arg.arg_id for arg in applicable_args]
         logging.debug('Checking applicability and weights of {}'.format(applic_arg_ids))
         weights = [self.weight_of(argument) for argument in applicable_args]
         logging.debug('Weights of {} are {}'.format(applic_arg_ids, weights))
         return max(weights)
-    
+
     def max_weight_pro(self, proposition):
         args = self.argset.get_arguments(proposition)
         return self.max_weight_applicable(args)
-    
+
     def max_weight_con(self, proposition):
         args = self.argset.get_arguments(proposition.negate())
         return self.max_weight_applicable(args)    
-        
 
 
-DOCTEST = 0
+
+DOCTEST = 1
 
 def arg_test():
     kill = PropLiteral('kill')
@@ -515,28 +502,29 @@ def arg_test():
     unreliable1 = PropLiteral('unreliable1')
     witness2 = PropLiteral('witness2')
     unreliable2 = PropLiteral('unreliable2')
-    
+
     ps = ProofStandard([(intent, "beyond_reasonable_doubt")], default='scintilla')
-    
+
     arg1 = Argument(murder, premises={kill, intent}, arg_id='arg1')
     arg2 = Argument(intent, premises={witness1}, exceptions={unreliable1}, arg_id='arg2')
     arg3 = Argument(neg_intent, premises={witness2}, exceptions={unreliable2}, arg_id='arg3')
-    
+
     argset = ArgumentSet()
     argset.add_argument(arg1)
     argset.add_argument(arg2)
     argset.add_argument(arg3)
-    #argset.draw(debug=True)
+    argset.draw(debug=True)
 
     #for arg in argset.get_arguments(intent): 
         #print(arg)
 
-        
+
     assumptions = {kill, witness1, witness2, unreliable2}
     weights = {'arg1': 0.8, 'arg2': 0.3, 'arg3': 0.8}
     audience = Audience(assumptions, weights)
     caes = CAES(argset, audience, ps)    
     caes.acceptable(murder)
+    caes.acceptable(murder.negate())
 
 def graph_test():
 
@@ -566,4 +554,3 @@ if __name__ == '__main__':
         doctest.testmod(optionflags=doctest.NORMALIZE_WHITESPACE)
     else:
         arg_test()
-
