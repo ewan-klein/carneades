85f409e5d91d0fb6c477b253ef097e50b6ee19d2
Ewan Klein <ewan@inf.ed.ac.uk>
2014-09-22 22:01:03 +0100

    Restored stream call. Added docstring.

--- src/caes/tracecalls.py
+++ src/caes/tracecalls.py
@@ -1,14 +1,19 @@
 # Author: Eli Bendersky <eliben@gmail.com>
 #         Ewan Klein <ewan@inf.ed.ac.uk> (modifications)
-# 
+#
+# License: in public domain
 # See http://eli.thegreenplace.net/2012/08/22/easy-tracing-of-nested-function-calls-in-python
 
+"""
+Module to see what calls actually occurred during execution, their arguments
+and return values, when executing algorithms with complex function call
+sequences, and especially ones that require recursion.
 
+The class :class:`TraceCalls` is called as decorator :func:`@TraceCalls`.
+"""
 import sys
 from functools import wraps
 import logging
-
-logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.DEBUG)
 
 class TraceCalls(object):
     """ 
@@ -17,10 +22,11 @@ class TraceCalls(object):
     depth.
         
     """
-    def __init__(self, indent_step=2, show_ret=True):
+    def __init__(self, stream=sys.stdout, indent_step=2, show_ret=True):
         self.indent_step = indent_step
         self.show_ret = show_ret
         TraceCalls.cur_indent = 0
+        self.stream = stream
 
     def __call__(self, fn):
         @wraps(fn)
@@ -28,14 +34,14 @@ class TraceCalls(object):
             indent = ' ' * TraceCalls.cur_indent
             argstr = ', '.join(
                 [str(a) for a in args][1:])
-            logging.debug("{}Calling {}({})".format(indent, fn.__name__, argstr))
+            self.stream.write("\n{}Calling {}({})\n".format(indent, fn.__name__, argstr))
 
             TraceCalls.cur_indent += self.indent_step
             ret = fn(*args, **kwargs)
             TraceCalls.cur_indent -= self.indent_step
 
             if self.show_ret:
-                logging.debug("{}{}({})-->{}\n".format(indent, fn.__name__,
+                self.stream.write("{}{}({})-->{}\n".format(indent, fn.__name__,
                                                       argstr, ret))
                 return ret
         return wrapper
